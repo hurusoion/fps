7b7231c13461a9e8955f0f140ebb0ccfb007b274
hurusoion <hurusoionkim@gmail.com>
2024-06-09 14:06:51 +0800

    2

--- Assets/scrip b/Building.cs
+++ Assets/scrip b/Building.cs
@@ -1,12 +1,1 @@
-﻿using UnityEngine;
-
-public class Building : MonoBehaviour
-{
-    public string buildingName;
-    public int buildCost;
-
-    public void Build()
-    {
-        // 建造逻辑，例如消耗资源，生成建筑物
-    }
-}
+﻿
